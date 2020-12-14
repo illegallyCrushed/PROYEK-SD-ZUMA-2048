cf58cc8d628cc4329d86dd5e7da63b7a9e660605
illegallyCrushed <illegallyCrushed@github.com>
2020-12-14 21:45:47 +0700

    Test

--- Zuma 2048/Source.cpp
+++ Zuma 2048/Source.cpp
@@ -4,7 +4,7 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Blue);
 
     while (window.isOpen())
     {
@@ -19,8 +19,6 @@ int main()
         window.draw(shape);
         window.display();
     }
-
-
 
     return 0;
 }
