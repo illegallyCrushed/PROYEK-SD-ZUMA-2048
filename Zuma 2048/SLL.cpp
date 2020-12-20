36b83ade91413cfd23af1ce14c41b16bd10495e5
Timothy D <timothyhtomit@users.noreply.github.com>
2020-12-20 21:42:44 +0700

    SLL class

--- Zuma 2048/SLL.cpp
+++ Zuma 2048/SLL.cpp
@@ -1,1 +1,153 @@
 #include "SLL.h"
+
+Node::Node(int data, string color) {
+	Data = data; Color = color; Next = NULL;
+}
+
+void Node::SetData(int data)
+{
+	Data = data;
+}
+
+void Node::SetColor(string color)
+{
+	Color = color;
+}
+
+void Node::SetNext(Node* next)
+{
+	Next = next;
+}
+
+int Node::GetData()
+{
+	return Data;
+}
+
+string Node::GetColor()
+{
+	return Color;
+}
+
+Node* Node::GetNext()
+{
+	return Next;
+}
+
+SLL::SLL()
+{
+	Head = Tail = NULL;
+	Size = 0;
+}
+
+Node* SLL::GetHead()
+{
+	return Head;
+}
+
+int SLL::GetSize()
+{
+	return Size;
+}
+
+void SLL::AddFront(int n, string color)
+{
+	if (Size == MaxSize)
+		return;
+
+    Node* temp = new Node;
+    temp->SetData(n);
+	temp->SetColor(color);
+    temp->SetNext(Head);
+    Head = temp;
+	Size++;
+}
+
+void SLL::AddMiddle(int index, int n, string color)
+{
+	if (Size == MaxSize)
+		return;
+
+	Node* temp = new Node;
+	temp->SetData(n);
+	temp->SetColor(color);
+	temp->SetNext(NULL);
+
+	Node* iteration = Head;
+
+	for (int i = 0; i < index; i++)
+		iteration = iteration->GetNext();
+
+	temp->SetNext(iteration->GetNext());
+	iteration->SetNext(temp);
+	Size++;
+}
+
+void SLL::AddBack(int n, string color)
+{
+	if (Size == MaxSize)
+		return;
+
+	Node* temp = new Node;
+	temp->SetData(n);
+	temp->SetColor(color);
+	temp->SetNext(NULL);
+
+	if (Head == NULL) {
+		Head = temp;
+		Tail = temp;
+	}
+	else {
+		Tail->SetNext(temp);
+		Tail = temp;
+	}
+	Size++;
+}
+
+void SLL::Delete(int index)
+{
+	if (index >= Size)
+		return;
+
+	Node* temp = Head;
+
+	if (index == 0) {
+		temp = temp->GetNext();
+		Head = temp;
+	}
+	else if (index == Size - 1) {
+		for (int i = 0; i < Size; i++)
+			temp = temp->GetNext();
+
+		temp->SetNext(NULL);
+	}
+	else {
+		for (int i = 0; i < index; i++)
+			temp = temp->GetNext();
+
+		temp->SetNext(temp->GetNext()->GetNext());
+	}
+	Size--;
+}
+
+void SLL::Edit(int index, int n)
+{
+	if (index >= Size)
+		return;
+
+	Node* iteration = Head;
+
+	for (int i = 0; i < index; i++)
+		iteration = iteration->GetNext();
+
+	iteration->SetData(n);
+}
+
+void SLL::DeleteCombo(int start, int end)
+{
+	if (start < 0 || start >= Size || end < 0 || end >= Size)
+		return;
+
+	for (int i = start; i <= end; i++)
+		Delete(i);
+}
--- Zuma 2048/SLL.h
+++ Zuma 2048/SLL.h
@@ -1,5 +1,37 @@
+#include <string>
+using namespace std;
 #pragma once
-class SLL {
 
+class Node {
+private:
+	int Data;
+	string Color;
+	Node* Next;
+public:
+	Node(int data = 1, string color = "red");
+	void SetData (int data = 1);
+	void SetColor(string color = "red");
+	void SetNext(Node* next);
+	int GetData();
+	string GetColor();
+	Node* GetNext();
+};
+
+class SLL {
+private:
+	Node* Head;
+	Node* Tail;
+	int MaxSize = 10;
+	int Size;
+public:
+	SLL();
+	Node* GetHead();
+	int GetSize();
+	void AddFront(int n, string color);
+	void AddMiddle(int index, int n, string color);
+	void AddBack(int n, string color);
+	void Delete(int index);
+	void Edit(int index, int n);
+	void DeleteCombo(int start, int end);
 };
 
